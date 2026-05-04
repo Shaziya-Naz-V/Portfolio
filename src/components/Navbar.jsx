fba53f50ea7211649a316b48cd7d45c928835e4a
Shaziya-Naz-V <shazgitit@gmail.com>
2026-05-04 12:40:20 +0530

    update to responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,33 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-function nav() {
-    return (
-        <div className="navcontainer">
-            <div className="name">
-                <h2>Shaziya Naz .V</h2>
-            </div>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/skills">Skills</Link>
-                <Link to="/Project">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </div>
-        </div>
-    );
+import { useState } from 'react';
+
+function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="navcontainer">
+      <div className="name">
+        <h2>Shaziya Naz .V</h2>
+      </div>
+
+      {/* Hamburger Button */}
+      <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Nav Links */}
+      <div className={`links ${menuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+        <Link to="/skills" onClick={() => setMenuOpen(false)}>Skills</Link>
+        <Link to="/Project" onClick={() => setMenuOpen(false)}>Projects</Link>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+      </div>
+    </div>
+  );
 }
-export default nav;
+
+export default Nav;
